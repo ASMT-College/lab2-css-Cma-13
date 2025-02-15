f387216b85827355307820bef19eb0a372177007
Cma-13 <aasmasingdan77@gmail.com>
2025-02-15 15:40:51 +0545

    CSS Assignment

---
+++ CSS/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="background: red;">I am div
+        <div style="min-height: 100px; position: absolute;bottom: -5px">I am div 2</div>
+    </div>
+    <button>Button</button>
+    <style>
+        button{
+            position: relative;
+            bottom: 40px;
+            right: 20px;
+        }
+    </style>
+</body>
+</html>
---
+++ facebook/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <button class="btn">Sign Up</button><br><br>
+    <nav class="navbar">
+
+        <img src="logo.png" alt="">
+        <ul>
+            <li><a href="">Home</a> </li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Post</a></li>
+        </ul>
+    </nav>
+    <br>
+</header>
+<section>
+<form action="" id="myForm">
+      <legend>Login</legend>
+      <br>
+<label for="email">Email</label>
+<input type="email" id="email" name="email">
+<br>
+<br>
+<label for="password">Password</label>
+<input type="password" name="password" id="password"><br>
+<br>
+<button>Login</button>
+<br><br>
+Don't have account? <button>Create a new account</button>
+    </form>
+</section>
+<footer>Copyright@ Seema Project 
+    <script>document.write(new Date().getFullYear())</script>
+     <!-- <?php echo date('Y');?> -->
+</footer>
+</body>
+</html>
